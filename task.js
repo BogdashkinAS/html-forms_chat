82ebcac50544ce92c5648a46470a2447ba0698aa
Alexander Bogdashkin <a_bogdashkin@mail.ru>
2023-07-25 20:03:05 +0300

    add new homework

--- task.js
+++ task.js
@@ -11,60 +11,36 @@ function getTime() {
 }
 
 function getText() {
-    const arrayText = [`
+    let dateNew = getTime()
+    const arrayText = ['Добрый день, мы ещё не проснулись. Позвоните через 10 лет', 'Здравствуйте и пока!', 'Пока мы не хотим с вами разговаривать']
+    let random = arrayText[Math.floor(Math.random() * arrayText.length)]
+    let setText = `
     <div class="message">
-        <div class="message__time">09:21</div>
+        <div class="message__time">${dateNew}</div>
         <div class="message__text">
-        Добрый день, мы ещё не проснулись. Позвоните через 10 лет
+        ${random}
         </div>
-    </div>`, `
-    <div class="message">
-        <div class="message__time">09:21</div>
-        <div class="message__text">
-        Здравствуйте и пока!
-        </div>
-    </div>`, `
-    <div class="message">
-        <div class="message__time">09:21</div>
-        <div class="message__text">
-        Пока мы не хотим с вами разговаривать
-        </div>
-    </div>`]
-    let random = Math.floor(Math.random() * 3)
-    return arrayText[random]
+    </div>`
+    return setText
 }
 
 function setMessages() {
-    count = 0
-    countText = 0
     inputMessages.addEventListener('keyup', (event) => {
         event.preventDefault()
         if (event.code == 'Enter' || event.code == 'NumpadEnter') {
             if (inputMessages.value != '') {
-                getTime()
                 let dateNew = getTime()
                 let text = inputMessages.value
                 inputMessages.value = ''
                 messages.innerHTML += `
                 <div class="message message_client">
-                    <div class="message__time">21:10</div>
-                    <div class="message__text">Добрый день! Хочу купить розы для любимой!</div>
+                    <div class="message__time">${dateNew}</div>
+                    <div class="message__text">${text}</div>
                 </div>
                 `
-                let messageTime = document.querySelectorAll('.message__time')
-                let messageText = document.querySelectorAll('.message__text')
-                messageTime[count].textContent = dateNew
-                count++
-                messageText[countText].textContent = text
-                countText++
-
-                getText()
                 let textNew = getText()
                 messages.innerHTML += textNew
-                messageTime = document.querySelectorAll('.message__time')
-                messageTime[count].textContent = dateNew
-                count++
-                countText++
+                messages.scrollIntoView(false)
             }
         }
     })
